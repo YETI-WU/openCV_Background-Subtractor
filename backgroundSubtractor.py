bb656cb37848775b839df09413c5b485a27f43ec
YETI-WU <34374815+YETI-WU@users.noreply.github.com>
2018-05-30 10:33:19 -0500

    Update backgroundSubtractor.py

--- backgroundSubtractor.py
+++ backgroundSubtractor.py
@@ -4,18 +4,15 @@ backgroundSubtractor
 1. MOG -- Gaussian Mixture-based Background/Foreground Segmentation Algorithm. “An improved adaptive background mixture model for real-time tracking with shadow detection” in 2001. by P. KadewTraKuPong and R. Bowden
 2. MOG2 -- Gaussian Mixture-based Background/Foreground Segmentation Algorithm. “Improved adaptive Gausian mixture model for background subtraction” in 2004 and “Efficient Adaptive Density Estimation per Image Pixel for the Task of Background Subtraction” in 2006. by Z.Zivkovic
 3. GMG -- Algorithm combines statistical background image estimation and per-pixel Bayesian segmentation. “Visual Tracking of Human Visitors under Variable-Lighting Conditions for a Responsive Audio Art Installation” in 2012. by Andrew B. Godbehere et al.
-
 @author: Yen
 """
 
 import cv2
 
-
-cap = cv2.VideoCapture('sf_Lombard_street_3.mp4')
+cap = cv2.VideoCapture('Lombard Street San Francisco California USA Collage Video - youtube.comtanvideo11.wmv') # video shared by youtube.comtanvideo11
 fgbg_MOG = cv2.bgsegm.createBackgroundSubtractorMOG()
 fgbg_MOG2 = cv2.createBackgroundSubtractorMOG2()
 fgbg_GMG = cv2.bgsegm.createBackgroundSubtractorGMG() # SLOW
-
 
 while True:
     ret, frame = cap.read()
